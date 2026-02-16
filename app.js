3a1353101dc5c719b49b9aced1ad2dbbb46d0c48
RadmiloM <markovicradmilo35@gmail.com>
2026-02-17 00:02:55 +0100

    modal created style changed

---
+++ app.js
@@ -0,0 +1,6 @@
+const openModalButton = document.querySelector('.open-modal');
+const modal = document.querySelector('.modal');
+
+openModalButton.addEventListener('click', () => {
+    modal.showModal();
+})
