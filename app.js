cadcde8c7b0e259f669f63021a02204f58ba83bc
RadmiloM <markovicradmilo35@gmail.com>
2026-02-17 10:34:28 +0100

    added close button styles

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const openModalButton = document.querySelector('.open-modal');
 const modal = document.querySelector('.modal');
+const closeModalButton = document.querySelector('.close-button');
+console.log("🚀 ~ closeModalButton:", closeModalButton)
 
 openModalButton.addEventListener('click', () => {
     modal.showModal();
+})
+
+closeModalButton.addEventListener('click', () => {
+    modal.close();
 })
